acdb04b63d1b4c190a920c234eb8fece227e5b96
Dragos Catarahia <dragos.catarahia@gmail.com>
2013-11-19 18:04:20 +0200

    remove commented code

--- flis/auth.py
+++ flis/auth.py
@@ -34,23 +34,3 @@ def edit_is_allowed(f, check_country=False):
                 return render(request, 'restricted.html')
         return f(request, *args, **kwargs)
     return wrapper
-
-
-# def edit_is_allowed(func):
-#     @wraps(func)
-#     def wrapper(*args, **kwargs):
-#         request = args[0]
-
-#         if not getattr(settings, 'SKIP_EDIT_AUTHORIZATION', False):
-#             roles = getattr(request, 'user_roles', [])
-#             for role in roles:
-#                 if role in EDITOR_ROLES:
-#                     return func(*args, **kwargs)
-#             groups = getattr(request, 'user_groups', [])
-#             country = request.country.iso
-#             for group in groups:
-#                 if group[0] in COUNTRY_ADMINS[country]:
-#                     return func(*args, **kwargs)
-#             return render(request, 'restricted.html')
-#         return func(*args, **kwargs)
-#     return wrapper
